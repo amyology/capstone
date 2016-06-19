ee44df3281796e38232353ae19943dac71dc7121
Amy Chan <chamy09@gmail.com>
2016-06-19 02:18:44 -0500

    added validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,11 @@
 class Product < ActiveRecord::Base
 
+  validates :name, presence: true, length: { maximum: 20 }
+  validates :brand, presence: true, length: { maximum: 40 }
+  validates :product_type, presence: true, length: { maximum: 20 }
+  validates :product_color, presence: true, length: { maximum: 20 }
+  validates :image, presence: true
+
   has_many :traincases
   has_many :wishlists
   has_many :users, through: :wishlists
--- app/models/tag.rb
+++ app/models/tag.rb
@@ -1,4 +1,6 @@
 class Tag < ActiveRecord::Base
+
+  validates :text, presence: true, length: { maximum: 20 }
   
   belongs_to :dupe
 
